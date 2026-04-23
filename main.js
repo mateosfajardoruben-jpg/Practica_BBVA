a6a2f90b1d958597cbc7ceeb2ea9a15eb34f34c5
mateosfajardoruben-jpg <mateosfajardoruben@gmail.com>
2026-04-23 12:21:25 +0200

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-import './components/bbva-header.js';
-import './components/bbva-menu.js';
-import './components/bbva-app.js'
-import './components/mostrar-usuarios.js';
-import './components/borrar-usuario.js'; 
-import './components/agregar-usuario.js';
-import './styles.css';
